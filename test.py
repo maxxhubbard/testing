cd49946900dc156907d15513384797ea40a4e376
trocupocu <133394729+trocupocu@users.noreply.github.com>
2025-05-09 09:48:55 +0300

    Update test.py

--- test.py
+++ test.py
@@ -124,11 +124,14 @@ with SB(uc=True, test=True) as sb:
     if not testtw() and not testkick():
         channel = os.getenv("CHANNEL")
         url = f'https://www.youtube.com/@{channel}/videos'
+        url = "https://www.thaiticketmajor.com/concert/"
         sb.uc_open_with_reconnect(url, 5)
+        sb.uc_click("button.btn-signin", 4.1)
+        sb.sleep(2)
         sb.uc_gui_click_captcha()
         sb.sleep(2)
         sb.uc_gui_handle_captcha()
         sb.sleep(2)
-        sb.uc_click('ytd-rich-item-renderer.style-scope', reconnect_time=4)
+        sb.uc_click("ytd-rich-item-renderer.style-scope", 4.1)
         sb.sleep(12)
             
