81fd45f5e93a1d8c0b587167c975d5dbcca39f4b
trocupocu <133394729+trocupocu@users.noreply.github.com>
2025-05-09 09:25:12 +0300

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ import requests
 
 def testtw():
     # Retrieve environment variables
-    channel = os.getenv("CHANNEL")
+    channel = "enardo"# os.getenv("CHANNEL")
     authorization = os.getenv("AUTHORIZATION")
     client_id = os.getenv("TCLIENTID")
 
