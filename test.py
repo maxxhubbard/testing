67a997c1bedbac07c2e0b17d1a210552a59c2e71
trocupocu <133394729+trocupocu@users.noreply.github.com>
2025-05-10 11:27:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -24,8 +24,6 @@ def stop_warp():
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while stopping WARP: {e}")
 
-def start_psiphon():
-    subprocess.run(["sudo", "psiphon", "&"], check=True)
 
 def start_warp():
     try:
@@ -180,7 +178,6 @@ with SB(uc=True, test=True) as sb:
                     break
             sb.quit_extra_driver()
         if not testtw() and not testkick() and time.time() - start_time < duration:
-            start_psiphon()
             start_warp()
             sb.sleep(15)
             driver2 = sb.get_new_driver(undetectable=True, proxy="socks5://127.0.0.1:1081")
